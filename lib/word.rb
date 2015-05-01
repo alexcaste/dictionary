f877fa06d03118117ff979b96e22d8078483fe3d
Alex Caste <alex.castaneda@gmail.com>
2015-05-01 09:44:49 -0700

    Adding save method and class method all.  These save a word as an object into a class array variable.  Adding passing spec for save method.

--- lib/word.rb
+++ lib/word.rb
@@ -1,8 +1,20 @@
 class Word
 
+    @@all_words = []
+
     attr_reader(:name)
 
     define_method(:initialize) do |attributes|
         @name = attributes.fetch(:name)
     end
+
+    define_singleton_method(:all) do
+        @@all_words
+    end
+
+    define_method(:save) do
+        @@all_words.push(self)
+    end
+
+
 end
