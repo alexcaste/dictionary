d78c1d46015758e12c5128f15a42013485d467af
Alex Caste <alex.castaneda@gmail.com>
2015-05-01 13:30:20 -0700

    Adding failing integration spec to add new definition to an existing word.

--- spec/int_spec.rb
+++ spec/int_spec.rb
@@ -15,6 +15,20 @@ describe('path for adding a word', {:type => :feature}) do
      visit('/')
      fill_in('user_word', :with => 'Apple')
      click_button('Add')
-     expect(page).to have_content('Dictionary')
+     expect(page).to have_content('Apple')
    end
  end
+
+ describe('path for adding definition to a word', {:type => :feature}) do
+     it('allows the user to add a definition to a word in the dictionary') do
+         visit('/')
+         fill_in('user_word', :with => 'Pear')
+         click_button('Add')
+         expect(page).to have_content('Pear')
+         click_link('Pear')
+         expect(page).to have_content('Here are')
+         fill_in('new_def', :with => 'A yummy fruit')
+         click_button('Add')
+         expect(page).to have_content('A yummy fruit')
+    end
+end
