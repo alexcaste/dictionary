b2ff7281ab0d7ad00fc668b9d1965f2714482197
Alex Caste <alex.castaneda@gmail.com>
2015-05-01 09:37:02 -0700

    Writing failing spec to return word name in word class

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -1,3 +1,16 @@
 require("rspec")
 require("word")
 require('pry')
+
+describe(Word) do
+    # before() do
+    #     Word.clear()
+    # end
+
+    describe('#name') do
+        it('returns the name of word') do
+        test_word = Word.new(:name => "hello")
+        expect(test_word.name()).to(eq("hello"))
+        end
+    end
+end
